3ce8d844b934176f341ffe04efd512662e7d77a7
Amir Malak <amirmalak77@gmail.com>
2025-12-14 16:11:08 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Layout from './Components/Layout/Layout'
 import Home from './Components/Home/Home'
 import Product from './Components/Product/Product'
@@ -8,14 +8,18 @@ import MakeUp from './Components/MakeUp/MakeUp'
 import Cosmetik from './Components/Cosmetik/Cosmetik'
 
 let x = createBrowserRouter([
-  {path:"" ,element:<Layout/> , children:[
-        {index:true , element:<MakeUp/>},
-    {path:"home" , element:<Home/>},
-    {path: "product" , element:<Product/>},
-  {path:"cosmetik", element:<Cosmetik/>}
+  {
+    path: "", element: <Layout />, children: [
+      { index: true, element: <MakeUp /> },
+      { path: "home", element: <Home /> },
+      { path: "product", element: <Product /> },
+      { path: "cosmetik", element: <Cosmetik /> }
 
-  ] }
-])
+    ]
+  }
+],{
+  basename:'MakeUp-Project'
+})
 
 
 function App() {
@@ -23,7 +27,7 @@ function App() {
 
   return (
     <>
-   <RouterProvider router={x} ></RouterProvider>
+      <RouterProvider router={x} ></RouterProvider>
     </>
   )
 }
